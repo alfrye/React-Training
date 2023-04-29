f84038fcb4439a6c93feaa44a56d0b8a17e09303
alfrye <alfrye@gmail.com>
2023-04-29 12:07:41 -0400

    es 6 subclasses

--- indecision-app/src/playground/es6-class-1.js
+++ indecision-app/src/playground/es6-class-1.js
@@ -13,10 +13,59 @@ class Person {
   }
  }
 
-const me = new Person('Alan Frye', 54);
-console.log(me.getGreeting());
-console.log(me.getDescription());
-const other = new Person();
-console.log(other.getGreeting());
-console.log(other.getDescription());
+// Student is subclass of Person
+ class Student extends Person {
+      constructor(name, age, major) {
+        super(name,age);
+        this.major = major;
+      }
+      hasMajor() {
+        return !!this.major
+      }
+      // overrides method from parent class
+      getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor) {
+            description = description + ` Their major is ${this.major}`;
+        }
+        return description;
+      }
+ }
 
+ class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name,age);
+        this.homeLocation = homeLocation;
+    }
+    // override method from parent
+    getGreeting() {
+     let greeting = super.getGreeting();
+     greeting += ` I'm visiting from ${this.homeLocation}`;
+     return greeting;
+    }
+ }
+
+// const me = new Student('Alan Frye', 54, 'Seattle');
+// //console.log(me.getGreeting());
+// console.log(me.hasMajor());
+// console.log(me.getDescription());
+// console.log(me,getGreeting());
+
+// const other = new Student();
+// //console.log(other.getGreeting());
+// console.log(other.getDescription());
+// console.log(other.hasMajor());
+// console.log(other.getGreeting())
+ 
+const me = new Traveler('Alan Frye', 54, 'Seattle');
+//console.log(me.getGreeting());
+//console.log(me.hasMajor());
+//console.log(me.getDescription());
+console.log(me.getGreeting());
+
+const other = new Traveler();
+//console.log(other.getGreeting());
+//console.log(other.getDescription());
+//console.log(other.hasMajor());
+console.log(other.getGreeting())
+ 
