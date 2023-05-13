b87200dc8c5c30449a702744cb2d3d27e4c87c07
alfrye <alfrye@gmail.com>
2023-05-13 08:56:15 -0400

    Learned how to pass jsx into a component and used the props.childern to display the passed int jsx. is sample in playground file inline-jsx-component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,6 +3,7 @@ import  React from 'react';
 import ReactDOM  from 'react-dom';
 import IndecisionApp from './component/Indecision';
  
+
  ReactDOM.render(<IndecisionApp  />, document.getElementById('app'));  
 
 
